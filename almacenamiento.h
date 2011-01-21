3f0783b4c29cf47737c82352f7a3b7704965b058
Victor De Ponte <throoze@gmail.com>
2011-01-21 19:02:13 -0430

    Definidas algunas estructuras y tipos...

--- almacenamiento.h
+++ almacenamiento.h
@@ -1,3 +1,23 @@
+#define TAMAX 10
+
 typedef struct {
-  
-  {
+  int trozo[TAMAX],ini,fin;
+  segmento *ant,*sig;
+} segmento
+
+typedef struct {
+  segmento *head,*tail;
+} hashLote
+
+typedef struct {
+  void contenido;
+  cajita sig,ant;
+} cajita
+
+typedef struct {
+  cajita *head,*tail;
+} lista
+
+int insertar(hashLote *lote, int num);
+
+segmento newSegmento(int hola);
