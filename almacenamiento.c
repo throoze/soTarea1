4d6e321f6a70e9f9e236790a2e8d3ff48af415b5
Victor De Ponte <throoze@gmail.com>
2011-01-26 00:34:07 -0430

    Corregidos muchos errores de compilación. Agregadas un par de funciones: estaen y get.

--- almacenamiento.c
+++ almacenamiento.c
@@ -1,20 +1,38 @@
 #include "almacenamiento.h"
+#ifndef STD
+#define STD
+#include <stdio.h>
+#define TRUE 1
+#define FALSE 0
+#define TAMAX 10
+#endif
 
 /*INICIO Funciones y Procedimientos referentes al tipo hashLote*/
-Segmento newSegmento() {
-  Segmento nuevo = (Segmento *) malloc(sizeof(Segmento));
-  if (nuevo) {
-    nuevo.ini = 1;
-    nuevo.ant = NULL;
-    nuevo.sig = NULL;
+Segmento  *newSegmento() {
+  Segmento *nuevo = (Segmento *) malloc(sizeof(Segmento));
+  if (nuevo == NULL) {
+    fprintf(stderr, "newSegmento: Error al hacer la reserva de memoria!!!\n");
+    exit(1);
+  } else {
+    nuevo->ini = 1;
+    nuevo->ant = NULL;
+    nuevo->sig = NULL;
     int i;
     for (i = 0 ; i < TAMAX; i++){
-      nuevo.trozo[i] = 0;
+      nuevo->trozo[i] = 0;
     }
     return nuevo;
+  }
+}
+
+HashLote *newHashLote(){
+  HashLote *nuevo = (HashLote *) malloc(sizeof(HashLote));
+  if (nuevo != NULL) {
+    nuevo->head = nuevo->tail = newSegmento();
+    return nuevo;
   } else {
-    fprintf(stderr, "newSegmento: Error al hacer la reserva de memoria!!!\n");
-    exit 1;
+    fprintf(stderr, "newHashLote: Error al hacer la reserva de memoria!!!\n");
+    exit(1);
   }
 }
 
@@ -39,32 +57,95 @@ int calcPosicion (int pos) {
 }
 
 int insertar (HashLote *lote, int pos, int num) {
-  Segmento inUse = lote.head;
+  Segmento *inUse = lote->head;
   while (TRUE) {
-    int fin = inUse.ini + (TAMAX - 1);
-    if ( inUse.ini <= pos && pos <= fin ) {
+    int fin = inUse->ini + (TAMAX - 1);
+    if ( inUse->ini <= pos && pos <= fin ) {
       break;
-    } else if  ( fin < pos && inUse.sig == NULL ) {
-      Segmento nuevo = newSegmento();
-      nuevo.ini = calcRango(pos);
-      inUse.sig = nuevo;
-      nuevo.ant = inUse;
+    } else if  ( fin < pos && inUse->sig == NULL ) {
+      Segmento *nuevo = newSegmento();
+      nuevo->ini = calcRango(pos);
+      inUse->sig = nuevo;
+      nuevo->ant = inUse;
       inUse = nuevo;
       break;
-    } else if (pos < inUse.ini && inUse.ant != NULL) {
-      Segmento nuevo = newSegmento();
-      nuevo.ini = calcRango(pos);
-      inUse.ant->sig = nuevo;
-      nuevo.ant = inUse.ant;
-      inUse.ant = nuevo;
-      nuevo.sig = inUse;
+    } else if (pos < inUse->ini && inUse->ant != NULL) {
+      Segmento *nuevo = newSegmento();
+      nuevo->ini = calcRango(pos);
+      inUse->ant->sig = nuevo;
+      nuevo->ant = inUse->ant;
+      inUse->ant = nuevo;
+      nuevo->sig = inUse;
       inUse = nuevo;
       break;
     }
-    inUse = inUse.sig;
+    inUse = inUse->sig;
   }
   int newPos = calcPosicion(pos);
-  inUse.trozo[newPos] = num;
+  inUse->trozo[newPos] = num;
   return 0;
 }
+
+int get(HashLote *lote, int pos){
+  Segmento *inUse = lote->head;
+  int caso;
+  while (TRUE) {
+    int fin = inUse->ini + (TAMAX - 1);
+    if ( inUse->ini <= pos && pos <= fin ) {
+      int newPos = calcPosicion(pos);
+      return inUse->trozo[newPos];
+    } else if  ( fin < pos && inUse->sig == NULL ) {
+      return -1;
+    } else if (pos < inUse->ini && inUse->ant != NULL) {
+      return -1;
+    }
+    inUse = inUse->sig;
+  }
+}
+
+int estaen(HashLote *lote, int pos){
+  Segmento *inUse = lote->head;
+  int caso;
+  while (TRUE) {
+    int fin = inUse->ini + (TAMAX - 1);
+    if ( inUse->ini <= pos && pos <= fin ) {
+      int newPos = calcPosicion(pos);
+      if (inUse->trozo[newPos] != 0) {
+	return 1;
+      } else {
+	return 0;
+      }
+    } else if  ( fin < pos && inUse->sig == NULL ) {
+      return 0;
+    } else if (pos < inUse->ini && inUse->ant != NULL) {
+      return 0;
+    }
+    inUse = inUse->sig;
+  }
+}
 /*FIN Funciones y Procedimientos referentes al tipo HashLote*/
+
+
+/*INICIO Funciones y Procedimientos referentes al tipo Lista*/
+Cajita *newCajita() {
+  Cajita *nueva = (Cajita *) malloc(sizeof(Cajita));
+  if (nueva != NULL) {
+    nueva->contenido = nueva->sig = nueva->ant = NULL;
+    return nueva;
+  } else {
+    fprintf(stderr, "newCajita: Error al hacer la reserva de memoria!!!\n");
+    exit(1);
+  }
+}
+
+Lista *newLista() {
+  Lista *nueva = (Lista *) malloc(sizeof(Lista));
+  if (nueva != NULL) {
+    nueva->head = nueva->tail = NULL;
+    return nueva;
+  } else {
+    fprintf(stderr, "newLista: Error al hacer la reserva de memoria!!!\n");
+    exit(1);
+  }
+}
+/*FIN Funciones y Procedimientos referentes al tipo Lista*/
--- almacenamiento.h
+++ almacenamiento.h
@@ -3,13 +3,15 @@
 #include <stdio.h>
 #define TRUE 1
 #define FALSE 0
-#endif
 #define TAMAX 10
+#endif
+
+/*INICIO DEFINICIÓN DE TIPOS*/
 
 /*INICIO definición del tipo HashLote*/
-typedef struct {
+typedef struct segmento {
   int trozo[TAMAX],ini;
-  struct Segmento *ant,*sig;
+  struct segmento *ant,*sig;
 } Segmento;
 
 typedef struct {
@@ -18,9 +20,9 @@ typedef struct {
 /*FIN del tipo HashLote*/
 
 /*INICIO definición del tipo Lista.*/
-typedef struct {
-  void contenido;
-  struct Cajita sig,ant;
+typedef struct cajita{
+  void *contenido;
+  struct cajita *sig,*ant;
 } Cajita;
 
 typedef struct {
@@ -28,7 +30,11 @@ typedef struct {
 } Lista;
 /*FIN del tipo Lista.*/
 
+/* FIN DEFINICIÓN DE TIPOS*/
 
+/*----------------------------------------------------------------------------*/
+
+/*INICIO DECLARACIÓN DE FUNCIONES Y PROCEDIMIENTOS*/
 
 /*INICIO Funciones y Procedimientos referentes al tipo hashLote*/
 /**
@@ -36,25 +42,18 @@ typedef struct {
  * inicializar los campos convenientemente. El contenido del arreglo (trozo) se 
  * inicializa en 0.
  *
- * retorna: un Segmento nuevo, inicializado, con su correspondiente reserva de 
- *          memoria.
+ * retorna: un apuntador a Segmento nuevo, inicializado, con su correspondiente 
+ * reserva de memoria.
  */
-Segmento newSegmento();
-
+Segmento *newSegmento();
 
 /**
- * Inserta el numero 'num' en la posición 'pos' del HashLote '*lote'.
+ * Constructor del tipo HashLote. Se encarga de hacer la reserva de memoria.
  * 
- * *lote: apuntador al lote donde se desea hacer la inserción.
- * 
- * pos: posición en la que se desea insertar.
- * 
- * num: número a insertar en el HashLote.
- * 
- * retorna: Un entero que indica el estado de la inserción; 0 si fue realizada 
- *          con éxito, cualquier otro número en caso contrario.
+ * retorna: un nuevo apuntador a HashLote con su correspondiente reserva de 
+ * memoria.
  */
-int insertar(HashLote *lote, int pos, int num);
+HashLote *newHashLote();
 
 /**
  * Sirve para calcular el Segmento al que corresponde la posicion pasada como 
@@ -78,7 +77,69 @@ int calcRango(int pos);
  *          posición 'pos'.
  */
 int calcPosicion(int pos);
+
+/**
+ * Inserta el numero 'num' en la posición 'pos' del HashLote '*lote'.
+ * 
+ * *lote: apuntador al lote donde se desea hacer la inserción.
+ * 
+ * pos: posición en la que se desea insertar.
+ * 
+ * num: número a insertar en el HashLote.
+ * 
+ * retorna: Un entero que indica el estado de la inserción; 0 si fue realizada 
+ *          con éxito, cualquier otro número en caso contrario.
+ */
+int insertar(HashLote *lote, int pos, int num);
+
+/**
+ * Devuelve el elemento almacenado en la posición 'pos', en el HashLote 'lote'.
+ * 
+ * *lote: apuntador al lote donde se buscará el elemento.
+ * 
+ * pos: posición que se desea buscar  en el HashLote *lote.
+ * 
+ * retorna: el elemento en el HashLote '*lote' en la posición 'pos', en caso de
+ *          estar. Devuelve 0 si el elemento en la posición 'pos' no ha sido 
+ *          asignado; y -1 en caso de que el segmento correspondiente a la 
+ *          posición buscada no exista.
+ */
+int get(HashLote *lote, int pos);
+
+
+/**
+ * Dice si el elemento en la posición 'pos' ha sido insertado en el HashLote 
+ * '*lote'.
+ * *lote: apuntador al lote donde se buscará el elemento.
+ * 
+ * pos: posición que se desea buscar en el HashLote *lote.
+ * 
+ * retorna: 1 en caso de que el elemento en la posición 'pos' haya sido asignado;
+ *          0 en caso contrario.
+ */
+int estaen(HashLote *lote, int pos);
 /*FIN Funciones y Procedimientos referentes al tipo HashLote*/
 
+/*----------------------------------------------------------------------------*/
 
 /*INICIO Funciones y Procedimientos referentes al tipo Lista*/
+/**
+ * Crea una nueva Cajita, reservando la memoria necesaria para ello.
+ * 
+ * retorna: Un nuevo apuntador a Cajita vacía.
+ */
+Cajita *newCajita();
+
+/**
+ * Crea una nueva lista vacía, reservando la memoria necesaria para ello.
+ * 
+ * retorna: un nuevo apuntador a una lista vacía.
+ */
+Lista *newLista();
+/*FIN Funciones y Procedimientos referentes al tipo Lista*/
+
+/*----------------------------------------------------------------------------*/
+
+
+/*FIN DECLARACIÓN DE FUNCIONES Y PROCEDIMIENTOS*/
+/*FIN DEL ARCHIVO (EOF)*/
