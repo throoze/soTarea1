9c3f33e536aa94d60dd97029f0e12d624fda6086
narval <isaaclpe@gmail.com>
2011-01-31 01:17:33 +0000

    casi listo

--- main.c
+++ main.c
@@ -14,43 +14,57 @@
 
 int main( int argc, char *argv[]) {
   /* INICIALIZACIONES */
+  printf("hola0");
   FILE *entrada = fopen(argv[1], "r");
   HashLote cand_raiz = *(newHashLote());
   HashLote hijos = *(newHashLote());
   ListaInt casos = *(newListaInt());
   int fuente;
   int destino;
+  printf("hola1");
 
-  /* LECTURA DEL ARCHIVO DE ENTRADA*/
+  /* LECTURA DEL ARCHIVO DE ENTRADA */
   while(fscanf(entrada, "%d %d", &fuente, &destino) != EOF){
+    printf("algo");
     if (fuente != -1 && destino != -1) {
-      if (fuente != 0 && destino != 0)
-  	/* ALMACENAR LA ENTRADA Y DESCARTAR LAS DOS PRIMERAS CONDICIONES */
+       if (fuente != 0 && destino != 0){
+      	printf("algo"); 
 
-	/* INSERTA LA FUENTE EN LOS CANDIDATOS DE RAIZ SOLO SI NO EXISTIA YA EN ESTE HASHLOTE O EN HIJOS*/
-	if (!(contiene(&cand_raiz, fuente) && !(contiene(&hijos, fuente)))) {
-	  insertar(&cand_raiz, fuente, 1);  
+
+
+      	/* ALMACENAR LA ENTRADA Y DESCARTAR LAS DOS PRIMERAS CONDICIONES */
+  	/* INSERTA LA FUENTE EN LOS CANDIDATOS DE RAIZ SOLO SI NO EXISTIA YA EN ESTE HASHLOTE O EN HIJOS*/
+  	if (!(contiene(&cand_raiz, fuente) && !(contiene(&hijos, fuente)))) {
+  	  insertar(&cand_raiz, fuente, 1);
+  	}
+      
+
+  	/* INSERTA LOS HIJOS MIENTRAS NO ESTEN REPETIDOS EN HIJOS, SI SE ENCUENTRA COMO POSIBLE RAIZ, ENTONCES ES ELIMINADO DE DICHO HASHLOTE*\/ */
+  	if(!(contiene(&hijos, destino))){
+	  	  insertar(&hijos, destino, fuente);
+	  	  printf("hijo %d", destino);
+  	}
+	/* else{ */
+  	/*   add(&casos, 0); */
+  	/*   printf("HIJO 2 VECES\n"); */
+	/* } */
+	else{
+	  printf("otro caso");
+	  hl_liberar(&cand_raiz);
+	  hl_liberar(&hijos);
 	}
-	else {
 
-	  /* INSERTA LOS HIJOS MIENTRAS NO ESTEN REPETIDOS EN HIJOS, SI SE ENCUENTRA COMO POSIBLE RAIZ, ENTONCES ES ELIMINADO DE DICHO HASHLOTE*/
-	  if(!(contiene(&hijos, destino))){
-	    insertar(&hijos, destino, fuente);
-	  }else{
-		add(&casos, 0);
-	  }
-	  /* PROBAR LA CONDICION QUE FALTA E IMPRIMIR LA SALIDA DE ESTE CASO */
-        }
-      //    printf("%d %d\n", fuente, destino);
-     
-  } else {
-      break;
+
+       }
+       else {
+	 break;
+       }
     }
+    printf("\n POSIBLES RAICES\n");
+    hl_print(cand_raiz);
+    printf("\n HIJOS\n");
+    hl_print(hijos);
+    printf("\n CASOS\n");
+    li_print(casos);
   }
-  printf("POSIBLES RAICES");
-  hl_print(cand_raiz);
-  printf("HIJOS");
-  hl_print(hijos);
-  printf("CASOS");
-  li_print(casos); 
 }
