c635a9c54ca476a171721b28296aa9aa97ae056c
Victor De Ponte <throoze@gmail.com>
2011-01-30 20:17:58 -0430

    Esqueleto exteno del programa listo... falta chequear las condiciones de arbol de cada caso.

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #ifndef STD
 #define STD
 #include <stdio.h>
+#include <string.h>
 #define TRUE 1
 #define FALSE 0
 #define TAMAX 10
@@ -12,14 +13,27 @@
 #endif
 
 int main( int argc, char *argv[]) {
-
-  FILE *leer = fopen(argv[1], "r");
-  int fuente;
-  int destino;
-  while(fscanf(leer, "%d %d", &fuente, &destino) != EOF){
-    printf("%d %d\n", fuente, destino);
-  }
-
+  /* INICIALIZACIONES */
+  FILE *entrada = fopen(argv[1], "r");
   HashLote cand_raiz = *(newHashLote());
   HashLote hijos = *(newHashLote());
+  int fuente;
+  int destino;
+
+  /* LECTURA DEL ARCHIVO DE ENTRADA*/
+  while(fscanf(entrada, "%d %d", &fuente, &destino) != EOF){
+    if (fuente != -1 && destino != -1) {
+      if (fuente != 0 && destino != 0) 
+	/* ALMACENAR LA ENTRADA Y DESCARTAR LAS DOS PRIMERAS CONDICIONES */
+	if (TRUE) {
+	  
+	}
+      } else {
+	/* PROBAR LA CONDICION QUE FALTA E IMPRIMIR LA SALIDA DE ESTE CASO */
+      }
+      printf("%d %d\n", fuente, destino);
+    } else {
+      break;
+    }
+  }
 }
--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #ifndef STD
 #define STD
 #include <stdio.h>
+#include <string.h>
 #define TRUE 1
 #define FALSE 0
 #define TAMAX 10
@@ -10,12 +11,3 @@
 #define ALM
 #include "almacenamiento.h"
 #endif
-
-
-/**
- * Se encarga de leer y almacenar un caso de prueba a la vez, para su posterior
- * análisis.
- * 
- * retorna: 0 en caso de que la lectura sea exitosa, 1 en caso contrario.
- */
-int lectura();
